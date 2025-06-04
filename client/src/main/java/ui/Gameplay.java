01696133a84cfd66f7fec27648ebdd5f3ade92ec
Michael <mwinder6@byu.edu>
2025-06-04 09:05:11 -0600

    Implemented a method that pieceConverter method that reads in a board and outputs a two-dimensional array of strings for each piece

--- client/src/main/java/ui/Gameplay.java
+++ client/src/main/java/ui/Gameplay.java
@@ -5,6 +5,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Random;
 
 import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import ui.EscapeSequences.*;
 
 import static ui.EscapeSequences.*;
@@ -22,14 +25,25 @@ public class Gameplay {
     private static final String[] rows = {" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 "};
     private static final String[] whitePieces = {WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_ROOK, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK};
 
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        ChessBoard board = new ChessBoard();
+        out.print(ERASE_SCREEN);
+
+        drawHeaders(out);
+        drawChessBoard(out, board);
+        drawHeaders(out);
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
     public static void drawBoard(ChessBoard board) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
         out.print(ERASE_SCREEN);
 
         drawHeaders(out);
-
-        drawTicTacToeBoard(out);
+        drawChessBoard(out, board);
         drawHeaders(out);
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
@@ -62,37 +76,37 @@ public class Gameplay {
         setGray(out);
     }
 
-    private static void drawTicTacToeBoard(PrintStream out) {
-
+    private static void drawChessBoard(PrintStream out, ChessBoard board) {
+        String[][] boardStrings = pieceConverter(board);
         for (int boardRow = 0; boardRow < 8; ++boardRow) {
             boolean startColor = boardRow % 2 == 1;
-            drawChessRow(out, startColor, rows[boardRow], whitePieces);
+            drawChessRow(out, startColor, rows[boardRow], boardStrings[boardRow]);
         }
     }
 
-    private static void drawChessRow(PrintStream out, Boolean startWhite, String row, String[] pieces){
-       out.print(SET_BG_COLOR_LIGHT_GREY);
-       out.print(SET_TEXT_COLOR_BLACK);
-       out.print(row);
-       for (int boardCol = 0; boardCol < 8; ++boardCol) {
+    private static void drawChessRow(PrintStream out, Boolean startWhite, String row, String[] board) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_BLACK);
+        out.print(row);
+        for (int boardCol = 0; boardCol < 8; ++boardCol) {
             if (startWhite == true) {
                 setWhite(out);
-                if (boardCol % 2 == 0){
+                if (boardCol % 2 == 0) {
                     setBlack(out);
                 }
             } else {
                 setBlack(out);
-                if (boardCol % 2 == 0){
+                if (boardCol % 2 == 0) {
                     setWhite(out);
                 }
             }
-            out.print(pieces[boardCol]);
-       }
-       out.print(SET_BG_COLOR_LIGHT_GREY);
-       out.print(SET_TEXT_COLOR_BLACK);
-       out.print(row);
-       setGray(out);
-       out.println();
+            out.print(board[boardCol]);
+        }
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_BLACK);
+        out.print(row);
+        setGray(out);
+        out.println();
     }
 
 
@@ -116,12 +130,57 @@ public class Gameplay {
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
+    private static String[][] pieceConverter(ChessBoard board) {
+        String[][] boardStrings = new String[8][8];
+        for (int row = 1; row <= 8; row++) {
+            for (int col = 1; col <= 8; col++) {
+                ChessPosition position = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(position);
+                boardStrings[row - 1][col - 1] = pieceIntoString(piece);
+            }
+        }
+        return boardStrings;
+    }
 
-        out.print(player);
-
-        setWhite(out);
+    private static String pieceIntoString(ChessPiece piece) {
+        if (piece == null) {
+            return EMPTY;
+        } else if (piece.getPieceType() == ChessPiece.PieceType.ROOK) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return WHITE_ROOK;
+            } else {
+                return BLACK_ROOK;
+            }
+        } else if (piece.getPieceType() == ChessPiece.PieceType.QUEEN) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return WHITE_QUEEN;
+            } else {
+                return BLACK_QUEEN;
+            }
+        } else if (piece.getPieceType() == ChessPiece.PieceType.KNIGHT) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return WHITE_KNIGHT;
+            } else {
+                return BLACK_KNIGHT;
+            }
+        } else if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return WHITE_BISHOP;
+            } else {
+                return BLACK_BISHOP;
+            }
+        } else if (piece.getPieceType() == ChessPiece.PieceType.KING) {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return WHITE_KING;
+            } else {
+                return BLACK_KING;
+            }
+        } else {
+            if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                return WHITE_PAWN;
+            } else {
+                return BLACK_PAWN;
+            }
+        }
     }
 }
